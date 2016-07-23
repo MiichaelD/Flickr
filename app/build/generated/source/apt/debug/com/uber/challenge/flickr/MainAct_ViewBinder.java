5372333d2253bfc1077153b747dcbea1579b3668
Michael Duarte <michael.duarte@hotmail.com>
2016-07-23 14:23:35 -0700

    Bind Views with ButterKnife

---
+++ app/build/generated/source/apt/debug/com/uber/challenge/flickr/MainAct_ViewBinder.java
@@ -0,0 +1,15 @@
+// Generated code from Butter Knife. Do not modify!
+package com.uber.challenge.flickr;
+
+import butterknife.Unbinder;
+import butterknife.internal.Finder;
+import butterknife.internal.ViewBinder;
+import java.lang.Object;
+import java.lang.Override;
+
+public final class MainAct_ViewBinder implements ViewBinder<MainAct> {
+  @Override
+  public Unbinder bind(Finder finder, MainAct target, Object source) {
+    return new MainAct_ViewBinding<>(target, finder, source);
+  }
+}
